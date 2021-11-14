76032eb48c1f372407a1d18338f712c35a92bb31
livtecq <nishiki1202@gmail.com>
2021-11-15 02:24:10 +0900

    Commentモデルの作成とアソシエーションの設定

---
+++ docker-files/app/models/comment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  comment    :text(65535)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
+class Comment < ApplicationRecord
+  belongs_to :board
+end
